df09e0e24a99e2a5a30acdae33aac3abbf4a71c0
Nihala-Parveen <nihalaparveen538@gmail.com>
2023-09-21 12:23:14 +0530

    first commit

---
+++ ig.js
@@ -0,0 +1,16 @@
+function like(){
+    const redHeart=document.getElementById('red')
+    redHeart.className='fa-solid fa-heart fa-2xl ps-4 pt-4'
+    redHeart.style.color='red'
+}
+function share(){
+    alert('Search...')
+}
+function comment(){
+    alert('Comments')
+}
+function follow(){
+    const fol=document.getElementById('req')
+    fol.value='Requested'
+    fol.className='text-light bg-dark border-0'
+}
